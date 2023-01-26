8168e3c5b546bd3f0b4712a829bc854eedfc3f64
Konstantin Larionov <76406214+kl-2d@users.noreply.github.com>
2023-01-26 21:16:58 +0300

    Add files via upload

---
+++ ezQest/Program.cs
@@ -0,0 +1,10 @@
+﻿string value = "Volodimir Singapuk <singapuk.e03@gmail.com>";
+string userName = "";
+string email = "";
+string[] items = value.Split('<', '>');
+userName = items[0];
+email = items[1];
+
+Console.WriteLine($"Username: {userName}");
+Console.WriteLine($"User mail: {email}");
+
